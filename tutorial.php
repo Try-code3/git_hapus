5793575a38dc7599774abffaafa522104045d927
try-code3 <triyogaprasetya749@gmail.com>
2025-10-21 14:40:29 +0800

    memperbarui page tutorial

--- tutorial.php
+++ tutorial.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
   <link href="src/output.css" rel="stylesheet">
 </head>
 <body class="bg-emerald-800">
@@ -45,69 +46,69 @@
 <section class="px-6 py-10 bg-gray-50 ">
   <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3">
     <!-- Card 1 -->
-    <a href="link-tujuan.html" 
+    <a href= "https://youtu.be/N83PbwXM90o" target="_blank" rel="noopener noreferrer"
        class="block rounded-xl bg-white shadow-sm hover:shadow-lg transition duration-300 p-2 text-center mb-3">
       <div class="flex justify-center">
-        <img src="img/cover.jpg" 
-             alt="gambar tutorial" 
+        <img src="https://img.youtube.com/vi/N83PbwXM90o/hqdefault.jpg" 
+             alt = "Thumbnail video"
              class="rounded-xl w-11/12 h-48 object-cover">
       </div>
-      <p class="mt-4 font-semibold text-gray-900 mb-5">Judul tutorial 0</p>
+      <p class="mt-4 font-semibold text-gray-900 mb-5">Panas Bumi Sumber Energi Terbarukan</p>
     </a>
     
     <!-- card 2 -->
-     <a href="link-tujuan.html" 
+     <a href="https://youtu.be/80a8P7Hwrn0?si=HjnVb3XXsqYx0ZZk" target="_blank" rel="noopener noreferrer"
        class="block rounded-xl bg-white shadow-sm hover:shadow-lg transition duration-300 p-2 text-center mb-3">
       <div class="flex justify-center">
-        <img src="img/cover.jpg" 
-             alt="gambar tutorial" 
+        <img src="https://i.ytimg.com/vi/80a8P7Hwrn0/hqdefault.jpg" 
+             alt="Thumbnail video" 
              class="rounded-xl w-11/12 h-48 object-cover">
       </div>
-      <p class="mt-4 font-semibold text-gray-900 mb-5">Judul tutorial 1</p>
+      <p class="mt-4 font-semibold text-gray-900 mb-5">Mengenal Energi Terbarukan: Masa Depan Energi Bersih</p>
     </a>
 
      <!-- card 3 -->
-     <a href="link-tujuan.html" 
+     <a href="https://youtu.be/EJ--DT43DM4?si=N2xmvzoN2N_frH9m" target="_blank" rel="noopener noreferrer"
        class="block rounded-xl bg-white shadow-sm hover:shadow-lg transition duration-300 p-2 text-center mb-3">
       <div class="flex justify-center">
-        <img src="img/cover.jpg" 
+        <img src="https://i.ytimg.com/vi/EJ--DT43DM4/hqdefault.jpg" 
              alt="gambar tutorial" 
              class="rounded-xl w-11/12 h-48 object-cover">
       </div>
-      <p class="mt-4 font-semibold text-gray-900 mb-5">Judul tutorial 2</p>
+      <p class="mt-4 font-semibold text-gray-900 mb-5">Energi Terbarukan | Contoh & Manfaat - Sains</p>
     </a>
 
      <!-- card 4 -->
-     <a href="link-tujuan.html" 
+     <a href="https://youtu.be/pb6OTxIg3lE?si=NwKLZz_70C9avlSt" target="_blank" rel="noopener noreferrer"
        class="block rounded-xl bg-white shadow-sm hover:shadow-lg transition duration-300 p-2 text-center mb-3">
       <div class="flex justify-center">
-        <img src="img/cover.jpg" 
+        <img src="https://i.ytimg.com/vi/pb6OTxIg3lE/hqdefault.jpg" 
              alt="gambar tutorial" 
              class="rounded-xl w-11/12 h-48 object-cover">
       </div>
-      <p class="mt-4 font-semibold text-gray-900 mb-5">Judul tutorial 3</p>
+      <p class="mt-4 font-semibold text-gray-900 mb-5">Menghemat Energi di Rumah Kita</p>
     </a>
 
     <!-- card 5 -->
-     <a href="link-tujuan.html" 
+     <a href="https://youtu.be/BZMP9yhX6xg?si=hrq6jG05ZH60OQaA" target="_blank" rel="noopener noreferrer"
        class="block rounded-xl bg-white shadow-sm hover:shadow-lg transition duration-300 p-2 text-center mb-3">
       <div class="flex justify-center">
-        <img src="img/cover.jpg" 
+        <img src="https://i.ytimg.com/vi/BZMP9yhX6xg/hqdefault.jpg" 
              alt="gambar tutorial" 
              class="rounded-xl w-11/12 h-48 object-cover">
       </div>
-      <p class="mt-4 font-semibold text-gray-900 mb-5">Judul tutorial 4</p>
+      <p class="mt-4 font-semibold text-gray-900 mb-5">Teknologi Ramah Lingkungan di Kehidupan Kita</p>
     </a>
 
     <!-- card 6 -->
-     <a href="link-tujuan.html" 
+     <a href="https://youtu.be/6GiTJAu24FE?si=tthHhrLJAzLciJMF" target="_blank" rel="noopener noreferrer"
        class="block rounded-xl bg-white shadow-sm hover:shadow-lg transition duration-300 p-2 text-center mb-3">
       <div class="flex justify-center">
-        <img src="img/cover.jpg" 
+        <img src="https://i.ytimg.com/vi/6GiTJAu24FE/hqdefault.jpg" 
              alt="gambar tutorial" 
              class="rounded-xl w-11/12 h-48 object-cover">
       </div>
-      <p class="mt-4 font-semibold text-gray-900 mb-5">Judul tutorial 5</p>
+      <p class="mt-4 font-semibold text-gray-900 mb-5">Tantangan Besar Transisi Energi Bersih Terbarukan (EBT)</p>
     </a>
 
     <!-- Tambahkan card lain sesuai kebutuhan -->
